27aaa9260508991d1f981a4e13358476f68078d5
MerryGoblin <keller.alexandre0@gmail.com>
2018-03-23 15:13:53 +0100

    Introduction to neurotransmitters. Program actually bugged. Previous version is ok

---
+++ config/constants/neurotransmitters.js
@@ -0,0 +1,12 @@
+
+/** @namespace */
+var Life = Life || {};
+
+Life.Neurotransmitters = {
+	acetylcholine: 1,
+	dopamine: 2,
+	gaba: 3,
+	glutamate: 4,
+	noradrenalin: 5,
+	serotonin: 6
+}
--- config/models/neurons/t2.js
+++ config/models/neurons/t2.js
@@ -3,11 +3,11 @@
 var Life = Life || {};
 Life.Models = Life.Models || {};
 
-Life.Models.neuronT1 = {
+Life.Models.neuronT2 = {
 	distances: {
 		dendrites: 100, // 100 microns | 0.1 millimeters
 		axon: 10000
 	},
 	impulseSpeed: 12, // micrometers / microsecondes
-	neurotransmitter: Life.Neurotransmitters.gaba;
+	neurotransmitter: Life.Neurotransmitters.glutamate;
 }
